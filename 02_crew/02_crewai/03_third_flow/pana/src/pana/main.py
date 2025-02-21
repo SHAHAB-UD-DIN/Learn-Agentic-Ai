eb59f9138da8fea9906f8939561f10e9ffb2e48b
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-22 02:11:27 +0500

    Update main.py

--- 02_crew/02_crewai/03_third_flow/pana/src/pana/main.py
+++ 02_crew/02_crewai/03_third_flow/pana/src/pana/main.py
@@ -1,6 +1,8 @@
 from crewai.flow.flow import Flow, start, listen
 from litellm import completion
 from dotenv import load_dotenv, find_dotenv
+from pana.crews.teaching_crew.teaching_crew import TeachingCrew
+
 _: bool = load_dotenv(find_dotenv())
 
 class PanaFlow(Flow):
@@ -12,12 +14,23 @@ class PanaFlow(Flow):
             messages=[
                 {
                     "role": "user",
-                    "content": "return the most trending topic in AI World. only share the topic, no other text"
+                    "content": "return the most trending topic in AI World. only share the topic no other text"
                 }
             ]
         )
         self.state['topic'] = response['choices'][0]['message']['content']
         print(f"STEP 1 TOPIC: {self.state['topic']}")
+
+    @listen("generate_topic")
+    def generate_content(self):
+        print(" STEP 2: Generating content...")
+        result = TeachingCrew().crew().kickoff(
+            inputs={
+                "topic": self.state['topic']
+            }
+        )
+        print(result)
+        
 
 def kickoff():
     obj = PanaFlow()
