4ff9663f698e0a8c97817042f7b7851f7376c248
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-03-03 20:57:47 +0800

    create creawai project

---
+++ 03_crewai_online_class/class_13/multiple_agents1/src/multiple_agents/main1.py
@@ -0,0 +1,18 @@
+from crewai.flow import Flow, listen, start
+from multiple_agents.crews.dev_crew.dev_crew import DevCrew
+
+class DevCrewFlow(Flow):
+
+    @start()
+    def run_dev_crew(self):
+        output = DevCrew().crew().kickoff(
+            inputs={
+                "problem": "write python code for addition two numbers"
+            }
+        )
+        return output.raw
+    
+def kickoff():
+    dev_flow = DevCrewFlow()
+    result = dev_flow.kickoff()
+    print(result)
